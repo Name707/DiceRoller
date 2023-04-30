abf2211840bff7752583225c13e04a46d596aed6
Name707 <707VLAN@gmail.com>
2023-04-30 13:10:19 +0600

    #1 Изменена кнопка. ElevationButton. Применен цвет.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +12,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,7 +28,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,26 +76,33 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         else -> R.drawable.dice_6
     }
     Column(
-        modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally
-    ) {
+        modifier = Modifier.background(Color(0xFFC3FBD8)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
         Image(
             painter = painterResource(R.drawable.dice_1),
             contentDescription = result.toString()
         )
 
-        Spacer(modifier = Modifier.height(150.dp))
+        Spacer(modifier = Modifier.height(100.dp))
 
-        Button(
+        ElevatedButton(
             onClick = { result = (1..6).random() },
             modifier = Modifier
-                .height(80.dp)
-                .width(250.dp)
+                .height(36.dp)
+                .width(150.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFFFACD)
+            )
         ) {
             Text(
                 text = stringResource(R.string.roll),
-                fontSize = 25.sp,
-                color = Color.Cyan
+                fontSize = 12.sp,
+                color = Color(0xFFDDA0DD)
             )
         }
+
+        Spacer(modifier = Modifier.height(60.dp))
     }
 }
